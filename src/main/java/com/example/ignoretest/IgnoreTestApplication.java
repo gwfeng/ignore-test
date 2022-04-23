843f889830412a28d84dd71f8b5d852f0ae6a0a9
feng <fgw1215@163.com>
2022-04-23 12:41:06 +0800

    feat add

---
+++ src/main/java/com/example/ignoretest/IgnoreTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.ignoretest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgnoreTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IgnoreTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ignoretest/IgnoreTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ignoretest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgnoreTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
